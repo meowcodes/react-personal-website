5dacba25c8eb79ab032fa6928da66ff504e20adf
Lena <meowcodes@gmail.com>
2023-01-19 21:29:26 -0800

    update about

--- src/About.js
+++ src/About.js
@@ -9,8 +9,8 @@ class About extends Component {
           <span className="heading">*About me</span>
           <div className="About-box">
             <div className="text-box">
-              <span className="text">Hi, my name is Lena. I’m a designer-researcher turned developer from San Francisco Bay Area. Currently, I’m studying full-stack development at Rithm School.</span>
-              <span className="text">I graduated <b>UC Berkeley</b> with an architecture degree and studied developmental psychology at <b>San Francisco State University</b>. I love children (I'm a RIE-trained babysitter), playing volleyball, and diving in the tropics.</span>
+              <span className="text">Hi, my name is Lena. I’m a designer-researcher turned developer from San Francisco Bay Area. Currently, I’m a software engineer at Pinterest.</span>
+              <span className="text">I graduated <b>UC Berkeley</b> with an architecture degree and studied developmental psychology at <b>San Francisco State University</b>. I love hanging out with my pets, making pottery, and diving in the tropics.</span>
             </div>
             <div className="img-box">
               <img className="img-cat" src={cat} alt="cat" />
